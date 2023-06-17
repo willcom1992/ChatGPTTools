f43f5db5674b49a98711c09aea1f4c1dcd451126
willcom <takuma1992jpn@gmail.com>
2023-06-17 22:46:43 +0900

    :sparkles: main ファイルの更新

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import openai
 import os
 from dotenv import load_dotenv
+
+# .envファイルの読み込み
 load_dotenv()
 
-## openai.organization = os.environ.get("OPENAI_ORGANIZATION")
+# OpenAI APIキーの設定
 openai.api_key = os.environ.get("OPENAI_API_KEY")
-completion = openai.ChatCompletion.create(
+
+# チャットボットへのリクエストとレスポンス
+response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[{"role": "user", "content": "こんにちは！"}]
+    messages=[
+        {"role": "user", "content": "ハムスターについて教えて"},
+    ],
 )
-print(completion.choices[0].message.content)
 
-
-def print_hi(name):
-    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
-
-
-# ガター内の緑色のボタンを押すとスクリプトを実行します。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
+# レスポンスの表示
+print(response.choices[0]["message"]["content"].strip())
