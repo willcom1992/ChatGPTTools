c5738fe21b2f781c4786b0f1652ca7df96d82278
willcom <takuma1992jpn@gmail.com>
2023-06-19 11:12:12 +0900

    :sparkles: ボットの編集

--- StatefulConversationLoop.py
+++ StatefulConversationLoop.py
@@ -2,41 +2,29 @@ import openai
 import os
 from dotenv import load_dotenv
 
-# .envファイルの読み込み
+# 環境変数の読み込み
 load_dotenv()
-
 # OpenAI APIキーの設定
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-def chat_with_gpt3(messages):
+
+def chat_with_ai(messages):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
-
     return response.choices[0].message.content
 
-def main():
-    conversation = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "system", "content": "語尾に「はむ」をつけて返答してください"},
-    ]
+conversation = [
+    {"role": "system", "content": "語尾に「はむ」をつけて返答してください"},
+    {"role": "system", "content": "あなたの名前は「ライト」という名前の犬という設定で会話をしてください。"},
+]
 
-    while True:
-        print("You: ")
-        user_input = input()
-        if user_input.lower() == 'exit':
-            break
-
-        conversation.append({"role": "user", "content": user_input})
-
-        # GPT-3に対して会話データを送信する
-        response = chat_with_gpt3(conversation)
-
-        # 応答からテキストを抽出して表示する
-        print("AI: " + response)
-
-        conversation.append({"role": "assistant", "content": response})
-
-if __name__ == "__main__":
-    main()
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        break
+    conversation.append({"role": "user", "content": user_input})
+    response = chat_with_ai(conversation)
+    print("Bot: " + response)
+    conversation.append({"role": "assistant", "content": response})
